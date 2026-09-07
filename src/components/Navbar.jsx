702633ed8e432e6f8e27e1e3ab24f88104b16d7b
Khadija Muhammad Tasiu <dignifiedmt@gmail.com>
2026-09-07 13:56:09 +0100

    Add Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <ul className="navbar-list">
+        <li className="navbar-item"><a href="/">Home</a></li>
+        <li className="navbar-item"><a href="/about">About</a></li>
+        <li className="navbar-item"><a href="/contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+}
